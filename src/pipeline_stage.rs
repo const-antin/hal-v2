bdb7db6a10dbfa168f254bb8e28446a0210d9e2b
Konstantin Hoßfeld <konstantin@hossfeld.cc>
2024-10-15 11:14:36 -0700

    Add multiple internal target and source registers per ALU and simd lane to PipelineStage.

--- src/pipeline_stage.rs
+++ src/pipeline_stage.rs
@@ -4,55 +4,51 @@ use dam::structures::Time;
 use crate::{alu::{ALUInput, ALURtConfig}, scalar::Scalar};
 
 pub struct PipelineStage {
-    pub alu_configs: Vec<ALURtConfig>,
-    pub data: Vec<Scalar>,
-    pub delay: usize
+    pub alu_config: ALURtConfig, 
+    pub simd: usize,             // number of ALUs in the pipeline stage
+    pub register_depth: usize,   // number of target registers for each ALU
+    pub data: Vec<Vec<Scalar>>,  
 }
 
 impl PipelineStage {
-    pub fn new(alu_configs: Vec<ALURtConfig>) -> PipelineStage {
-        let len = alu_configs.len();
-        let delay = alu_configs
-            .iter()
-            .map(|c| c.op.delay())
-            .chain(std::iter::once(0))
-            .max()
-            .unwrap();
+    pub fn new(alu_config: ALURtConfig, simd: usize, register_depth: usize) -> PipelineStage {
 
         PipelineStage {
-            alu_configs: alu_configs,
-            data: vec![Scalar::I32(0); len],
-            delay: delay
+            alu_config: alu_config,
+            data: vec![vec![Scalar::I32(0); register_depth]; simd],
+            register_depth: register_depth,
+            simd: simd,
         }
     }
 
     // Returns time after pipeline stage completion
-    pub fn iter(&mut self, prev_stage: &Vec<Scalar>, time: Time) -> (&Vec<Scalar>, Time) {
-        assert_eq!(prev_stage.len(), self.data.len());
+    // The input is a Vec<Vec<Scalar>> because the PCU has multiple inputs. 
+    // For the 2nd..nth pipeline stage, the outer Vec is always of length 1.
 
-        let mut next_data = vec![Scalar::I32(0); self.data.len()];
+    // TODO: There are still many open questions here. For instance, we currently move values in the pipeline
+    // that are actually computed on by the ALU. We could, however, also move every value in the pipeline
+    // regardless of if it is computed on or not. The Plasticine paper does not describe how this is done. 
+    pub fn iter(&mut self, prev_stage: &Vec<Vec<Scalar>>, time: Time) -> (&Vec<Vec<Scalar>>, Time) {
+        let mut next_data = vec![vec![Scalar::I32(0); self.data.len()]];
 
-        for (idx, alu_config) in 
-                                                self.alu_configs.iter()
-                                                .enumerate() {
+        for idx in 0..self.simd {
 
-            let lhs = self.get_input(&alu_config.in_a, prev_stage, idx);
-            let rhs = self.get_input(&alu_config.in_b, prev_stage, idx);
+            let lhs = self.get_input(&self.alu_config.in_a, prev_stage, idx);
+            let rhs = self.get_input(&self.alu_config.in_b, prev_stage, idx);
 
-            next_data[idx] = alu_config.op.apply(&lhs, &rhs)
+            next_data[idx][self.alu_config.target] = self.alu_config.op.apply(&lhs, &rhs)
         }
         self.data = next_data;
-        (&self.data, time + self.delay as u64)
+        (&self.data, time + self.alu_config.op.delay() as u64)
     }
 
-    fn get_input(&self, alu_input: &ALUInput, prev_stage: &Vec<Scalar>, idx: usize) -> Scalar {
+    fn get_input(&self, alu_input: &ALUInput, prev_stage: &Vec<Vec<Scalar>>, idx: usize) -> Scalar {
         match alu_input {
-            ALUInput::NEXT => self.data[idx].clone(),
-            ALUInput::PREV => prev_stage[idx].clone(),
-            ALUInput::PREV_BELOW => {
-                assert!(idx + 1 < prev_stage.len(), r#"Selected ALU Input "PREV_BELOW" does not exist."#);
-                prev_stage[idx+1].clone()
-            }
+            ALUInput::NEXT(register_sel) => self.data[*register_sel][idx].clone(),
+            ALUInput::PREV(register_sel) => prev_stage[*register_sel][idx].clone(),
+            ALUInput::PREV_BELOW(register_sel) => prev_stage[*register_sel].get(idx+1)
+                .expect(r#"Selected ALU Input "PREV_BELOW" does not exist."#).clone(),
+            ALUInput::CONSTANT(x) => x.clone()
         }
     }
 }
@@ -67,41 +63,24 @@ mod tests {
 
     fn prepare() -> PipelineStage {
         let alu_rt_config_1 = ALURtConfig {
-            op: ALUOp::MUL_I32,
-            in_a: ALUInput::PREV,
-            in_b: ALUInput::PREV_BELOW
-        };
-
-        let alu_rt_config_2 = ALURtConfig {
             op: ALUOp::ADD_I32,
-            in_a: ALUInput::PREV,
-            in_b: ALUInput::NEXT
+            in_a: ALUInput::PREV(0),
+            in_b: ALUInput::NEXT(0),
+            target: 0
         };
-
-        PipelineStage::new(vec![alu_rt_config_1, alu_rt_config_2])
-    }
-
-    #[test]
-    fn correct_delay_test() {
-        let t = Time::new(0);
-        let mut pl = prepare();
-        let input = vec![Scalar::I32(1), Scalar::I32(2)];
-
-        let (_, t_d) = pl.iter(&input, t);
-        assert_eq!(t_d.time(), pl.alu_configs.iter().map(|x| x.op.delay()).max().unwrap() as u64);
+        PipelineStage::new(alu_rt_config_1, 1, 1)
     }
     
     #[test]
     fn pipeline_holds_state_test() {
         let t_0 = Time::new(0);
         let mut pl = prepare();
-        let input = vec![Scalar::I32(1), Scalar::I32(2)];
+        let input = vec![vec![Scalar::I32(1)]];
 
         let (_, t_1) = pl.iter(&input, t_0);
+        assert_eq!(pl.data[0][0], Scalar::I32(1));
         let _ = pl.iter(&input, t_1);
-
-        assert_eq!(pl.data[0], Scalar::I32(2));
-        assert_eq!(pl.data[1], Scalar::I32(4));
+        assert_eq!(pl.data[0][0], Scalar::I32(2));
     }
 
 }
